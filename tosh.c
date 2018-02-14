f3effa6a5724eea4e6af65b4d31e01748e8ad778
jamesponwith <jponwith@sandiego.edu>
2018-02-13 23:16:48 -0800

    execFunc

--- tosh.c
+++ tosh.c
@@ -18,7 +18,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
 // TODO: add your function prototypes here as necessary
 
 pid_t Fork(void);
@@ -27,22 +26,18 @@ void execCmd(char *argv[], int ret);
 void child_handler(__attribute__ ((unused)) int sig); 
 
 int main(){ 
-
 	
 	// TODO: add a call to signal to register your signal handler on 
 	//       SIGCHLD here
-	
 
 	struct sigaction sa;
 	sa.sa_handler = child_handler;
 	sa.sa_flags = SA_NOCLDSTOP;
 	sigaction(SIGCHLD, &sa, NULL);
 
-
 	while(1) {
 		// (1) read in the next command entered by the user
 		char *cmdline = readline("tosh$ ");
-
 		// NULL indicates EOF was reached, which in this case means someone
 		// probably typed in CTRL-d
 		if (cmdline == NULL) {
@@ -54,14 +49,10 @@ int main(){
 
 		// TODO: complete the following top-level steps
 		// (2) parse the cmdline
-
 		// (3) determine how to execute it, and then execute it
-
 	}
-
 	return 0;
 }
-
 
 /**
  * Prints out a message if an error has occured
@@ -73,8 +64,6 @@ void unix_error(char *msg) {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(0);
 }
-
-
 
 /*
  * Error handling for the fork() function
@@ -98,17 +87,15 @@ void execCmd(char *argv[], int ret) {
 	pid_t child_pid;
 	if((child_pid = Fork()) == 0) {
 		setpgid(0,0);
-		if(execv(argv[0], argv) == -1) {
+		if(execvp(argv[0], argv) == -1) {
 			fprintf(stdout, "ERROR: command not found\n");
 			exit(0);
 		}
 	}
-	else if {
+	else if (ret == 0) { // Foreground
 		waitpid(child_pid, &status, 0); // Wait for child
 	}
 }
-
-
 
 /*
  * Handles the child and makes the parent process wait
